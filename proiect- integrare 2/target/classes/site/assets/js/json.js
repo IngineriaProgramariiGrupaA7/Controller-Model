6887195a6eb3bc5b307b5932c7ec2586926fc177
CosminMoisii <cosmin.moisii@info.uaic.ro>
2016-05-19 02:03:35 +0300

    Add files via upload

---
+++ proiect- integrare 2/target/classes/site/assets/js/json.js
@@ -0,0 +1,13 @@
+var json = {
+	title: '',
+	description: '',
+	domain: '',
+	authors: {},
+	actorsAndObjectives: {},
+	stakeholdersAndInterests: {},
+	usecases: {},
+};
+
+var actorsAndObjectivesIndex = 0;
+var stakeholdersAndInterestsIndex = 0;
+var authorsIndex = 0;
